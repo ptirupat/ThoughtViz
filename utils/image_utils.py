aee82fbcc85c7f593a810078389591b54ed8b92a
Praveen <praveentirupattur@gmail.com>
2018-12-23 08:07:02 -0500

    Added code for combining grayscale images.

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -2,6 +2,20 @@ import numpy as np
 import math
 
 
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(math.sqrt(num))
+    height = int(math.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = img[:, :, 0]
+    return image
+
+
 def combine_rgb_images(generated_images):
     num = generated_images.shape[0]
     width = int(math.sqrt(num))
